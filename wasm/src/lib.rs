a8a1f3e3c6d750cc70ae9c8106ada0d2a6828318
Dustin Kerns <dkerns0123@gmail.com>
2025-06-10 08:38:36 -0700

    Write /workdir/wasm/src/lib.rs
    Add the rating_system module to lib.rs

--- wasm/src/lib.rs
+++ wasm/src/lib.rs
@@ -50,6 +50,7 @@ pub mod utils;
 pub mod js_interface;
 pub mod conversions;
 pub mod errors;
+pub mod rating_system;
 
 // Re-export optimized API
 pub use api::{WasmRating, WasmRatingSystem, WasmTeam};
